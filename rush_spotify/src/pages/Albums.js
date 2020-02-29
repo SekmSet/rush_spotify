c4c82eb8f75cebf1ff1d57890c018f44ec25c389
Priscilla Joly <priscilla.joly@epitech.eu>
2020-02-29 21:53:36 +0100

    front tracks react

---
+++ rush_spotify/src/pages/Albums.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Albums() {
+    return <h2>Page des Albums</h2>;
+}
+
+export default Albums;
---
+++ rush_spotify/src/pages/Artists.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Artists() {
+    return <h2>Page des artistes</h2>;
+}
+
+export default Artists;

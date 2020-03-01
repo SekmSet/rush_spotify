c1731b9b857c9c7f056bc672f8524443599e758b
Priscilla Joly <priscilla.joly@epitech.eu>
2020-03-01 18:05:57 +0100

    finish artists parts

--- rush_spotify/src/components/artistDetail.js
+++ rush_spotify/src/components/artistDetail.js
@@ -13,6 +13,23 @@ const ArtistDetail = ({ artist }) => {
                         <p className="card-text">
                             <img src={artist.photo} alt={artist.name} />
                         </p>
+
+                        {artist.albums != null && artist.albums.map((album) => (
+                            <div key={artist.id} className="card">
+                                <div className="card-body">
+                                    <h5 className="card-title">
+                                        <img src={album.cover_small}  alt="Album cover"/>
+
+                                        <Link to={`/albums/${album.id}`}>{album.name}</Link>
+
+                                        <p>Popumarité : {album.popularity}</p>
+                                        <p>Nombre de tracks : {album.tracks_number}</p>
+                                        <p>Année de réalisation : {album.release_year_date}</p>
+                                    </h5>
+                                </div>
+                            </div>
+                        ))}
+
                     </div>
                 </div>
         </div>
